baec4649517edbc397023d53b10840067e57f0ba
phantom0z <agnibha0016@gmail.com>
2021-06-30 09:26:33 +0530

    Render graphs through api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout.js';
 import virusImg from './img/covid19-1600x900_adobespark.png';
 import Evaluation from './routes/Evaluation/Evaluation';
+import Stat from './routes/Stat/Stat.js';
 const imgArr = [virusImg, virusImg, virusImg, virusImg, virusImg, virusImg];
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
 			<Switch>
 				<Layout>
 					<Route exact path='/' component={Evaluation} />
+					<Route exact path='/covid-overview' component={Stat} />
 					<span className='virus'>
 						{imgArr.map(img => {
 							return <img src={img} alt='' />;
---
+++ src/routes/Stat/Stat.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
+import axios from 'axios';
+import {
+	BarChart,
+	Bar,
+	Cell,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+} from 'recharts';
+import './stat.scss';
+
+const StatGraph = ({ data }) => (
+	<ResponsiveContainer width='100%' height={250}>
+		<BarChart
+			width={400}
+			height={300}
+			data={data}
+			margin={{
+				top: 5,
+				right: 30,
+				left: 20,
+				bottom: 5,
+			}}>
+			<CartesianGrid strokeDasharray='3 3' />
+			<XAxis dataKey='name' />
+			<YAxis />
+			<Tooltip />
+			<Legend />
+			<Bar dataKey='active' fill='#8884d8' />
+			<Bar dataKey='confirmed' fill='#82ca9d' />
+			<Bar dataKey='recovered' fill='#82ca9d' />
+		</BarChart>
+	</ResponsiveContainer>
+);
+
+const Stat = () => {
+	const [covidStateData, setCovidStateData] = useState({});
+	const [states, setStates] = useState([]);
+	const [districtData, setDistrictData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [err, setErr] = useState();
+	const customStyles = {
+		option: (provided, state) => ({
+			...provided,
+			// borderBottom: '1px dotted pink',
+			color: state.isSelected ? 'white' : 'blue',
+			padding: 20,
+		}),
+	};
+	const mockData = [
+		{
+			name: 'kolkata',
+			recovered: 4000,
+			dead: 2400,
+			amt: 2400,
+		},
+	];
+	const renderChart = stateName => {
+		let tempData = [];
+		let district = covidStateData[stateName].districtData;
+		for (const data in district) {
+			tempData.push({ name: data, ...district[data] });
+		}
+		setDistrictData(tempData);
+	};
+	console.log('district', districtData);
+
+	useEffect(() => {
+		setIsLoading(true);
+		axios
+			.get('https://api.covid19india.org/state_district_wise.json')
+			.then(covid => {
+				setCovidStateData(covid.data);
+				let tempState = [];
+				for (const state in covid.data) {
+					const option = { value: state, label: state };
+					tempState.push(option);
+				}
+				setStates(tempState);
+				// console.log(tempState);
+			})
+			.catch(err => setErr(err));
+		setIsLoading(false);
+	}, []);
+	return (
+		<div className='stat-main'>
+			<Select
+				className='basic-single'
+				classNamePrefix='select'
+				styles={customStyles}
+				defaultValue={states[4]}
+				isDisabled={isLoading}
+				isLoading={isLoading}
+				isClearable={true}
+				isSearchable={true}
+				name='states'
+				options={states}
+				onChange={state => renderChart(state.value)}
+			/>
+			<div className='chart-container'>
+				{districtData.length > 0 ? (
+					districtData.map(data => <StatGraph data={[data]} />)
+				) : (
+					<h1>No data to vusialize</h1>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Stat;
